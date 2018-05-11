3f5aa07adfd60e3ce9ed7da3a2e44b303c184723
Wilson Ortiz Troncoso <w.ortizgeek@gmail.com>
2018-05-10 23:53:27 -0300

    logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -81,4 +81,12 @@ export class AppComponent implements OnInit {
         }
       });
   }
+
+  logout() {
+    localStorage.removeItem('identity');
+    localStorage.removeItem('token');
+    localStorage.clear();
+    this.identity = null;
+    this.token = null;
+  }
 }
