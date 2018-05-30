a5a3fd058d7006c9c442588e11c718f43d52ead0
Wilson Ortiz Troncoso <w.ortizgeek@gmail.com>
2018-05-30 11:47:45 -0400

    update service artist: getArtists, getArtist, updateArtist, deleteArtist

--- src/app/services/artist.service.ts
+++ src/app/services/artist.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response,Headers,Request, RequestOptions } from '@angular/http';
+import { Http, Response, Headers, Request, RequestOptions } from '@angular/http';
 
 import { Observable } from 'rxjs';
 import { map } from "rxjs/operators";
@@ -18,6 +18,35 @@ export class ArtistService {
 		this.url = GLOBAL.url;
 	}
 
+	public getArtists(token,page){
+
+		let headers = new Headers({   
+			'Content-Type': 'application/json',
+			'Authorization': token
+		});
+
+		let options = new RequestOptions({headers:headers});
+
+		return this.http.get(this.url+'artists/'+ page,options).
+		pipe(map((res:Response) =>res.json()));
+
+	}
+
+
+	public getArtist(token,id:string){
+
+		let headers = new Headers({   
+			'Content-Type': 'application/json',
+			'Authorization': token
+		});
+
+		let options = new RequestOptions({headers:headers});
+
+		return this.http.get(this.url+'artist/'+id,options).
+		pipe(map((res:Response) =>res.json()));
+
+	}
+
 	public addArtist(token, artist:Artist){
 		let body = JSON.stringify(artist);
 		let headers = new Headers({   
@@ -29,5 +58,33 @@ export class ArtistService {
 
 		return this.http.post(this.url+'artist', body, options).
 		pipe(map((res:Response) => res.json()));
+	}
+
+
+	public uddateArtist(token,id:string, artist:Artist){
+		let body = JSON.stringify(artist);
+		let headers = new Headers({   
+			'Content-Type': 'application/json',
+			'Authorization': token
+		});
+		let options = new RequestOptions({headers:headers});
+
+
+		return this.http.put(this.url+'artist/'+id, body, options).
+		pipe(map((res:Response) => res.json()));
+	}
+
+	public deleteArtist(token,id:string){
+
+		let headers = new Headers({   
+			'Content-Type': 'application/json',
+			'Authorization': token
+		});
+
+		let options = new RequestOptions({headers:headers});
+
+		return this.http.delete(this.url+'artist/'+id,options).
+		pipe(map((res:Response) =>res.json()));
+
 	}
 }
